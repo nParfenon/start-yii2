65046548399a2d72a565d3169347a8fe5f370e44
nParfenon <nikita.parfenov.work@yandex.ru>
2022-12-09 21:47:21 +0800

    edit

--- modules/main/views/frontend/default/index.php
+++ modules/main/views/frontend/default/index.php
@@ -6,17 +6,4 @@ if (!Yii::$app->user->isGuest){
     echo 'YES';
 }
 
-$prefix = '_it';
-$id = 21;
-$time = time() + 60 * 30;
-
-$token = Yii::$app->security->generateRandomString() . $prefix . $id . $prefix . $time;
-
-$exp = explode($prefix, $token);
-var_dump(array_pop($exp));
-var_dump(array_pop($exp));
-var_dump($exp);
-var_dump($token);
-//var_dump(date('Y-m-d H:i:s',));
-die();
 ?>
